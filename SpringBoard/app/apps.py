a56984ee2c928c414467b16ce6ab6a48f8222f13
Junxiang <junxiang.ng.2016@smu.edu.sg>
2018-07-15 17:30:58 +0800

    reset django project

--- SpringBoard/app/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class SpringBoardConfig(AppConfig):
-    name = 'app'
--- SpringBoard/app/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from app.models import Login
-
-class LoginSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Login
-        fields = ('username', 'password')
-
