d357c1bbc2fcfaa9e3669f830f303ec0cf844264
Randy Lai YongHao <randylai.2016@sis.smu.edu.sg>
2018-07-21 16:37:37 +0800

    Authentication Token with timestamp

--- SpringBoard/app/views.py
+++ SpringBoard/app/views.py
@@ -18,6 +18,7 @@ from .models import *
 from pymongo import MongoClient
 import jwt
 from .userCRUD import *
+import datetime
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.SpringBoard
@@ -34,6 +35,21 @@ def isCM(userType):
 
 def isRM(userType):
     return userType == "RM"
+
+def tokenAuthenticate(username,token):
+    results = {}
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        if(not username == payload['username']):
+            results = {'error' : 'Invalid Token'}
+            return (results)
+    except(jwt.DecodeError):
+        results = {'error' : 'Invalid Token'}
+        return (results)
+    except(jwt.ExpiredSignatureError):
+        results = {'error' : 'Token has expired'}
+        return (results)
+    return results
 
 
 class UserLogin(CreateAPIView):
@@ -59,7 +75,12 @@ class UserLogin(CreateAPIView):
             results = {}
             results['userType'] = collection.find_one({'username':username},{'userType':1, '_id':0})['userType']
 
-            encoded_token = jwt.encode({'username': username}, SECRET_KEY, algorithm='HS256')
+            encoded_token = jwt.encode({
+                'username': username,
+                'iat': datetime.datetime.utcnow(),
+                'nbf': datetime.datetime.utcnow() + datetime.timedelta(minutes=-5),
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)
+                }, SECRET_KEY, algorithm='HS256')
             results['token'] = encoded_token
             return Response(results)
 
@@ -73,8 +94,9 @@ class RetrieveUsers(CreateAPIView):
         token = request.data['token']
         userType = request.data['userType']
 
-        if(not checkLogonStatus(username,token)):
-            return Response({'error' : 'Invalid token' })
+        results = tokenAuthenticate(username,token)
+        if(results[0] == "error"):
+            return Response(results)
         if(not isAdmin(userType)):
             return Response({'error' : 'invalid userType' })
 
