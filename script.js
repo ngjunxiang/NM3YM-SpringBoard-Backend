2dfeec5ed0fb5268eebb8e64c4f50c16ff4df1a7
Ellpeeaxe <peixuan.lim.2016@smu.edu.sg>
2018-08-04 11:13:57 +0800

    Changed DB setup to store password hashes instead

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ db.dropDatabase()
 // set up users table
 db.Users.createIndex({"username": 1 }, { unique: true } )
 db.Users.insertMany([
-	{"username": "admin","password": "admin123456","userType": "ADMIN", "email": "admin@email.com"},
-	{"username": "RandyLai","password": "rl123456","userType": "CM", "email": "randy@email.com"},
-	{"username": "LimPeiXuan","password": "lpx123456","userType": "RM", "email": "peixuan@email.com"}
+	{"username": "admin","password": "$argon2i$v=19$m=512,t=2,p=2$0OvvCnGQIgmrBhzF1DzRJg$AndnHTRF7unXj/g4NYFhzA","userType": "ADMIN", "email": "admin@email.com"},
+	{"username": "RandyLai","password": "$argon2i$v=19$m=512,t=2,p=2$xaPF+Umz4EJsi8KcfYcpFg$EYR0xPAIbJTLhmLU2IQC9A","userType": "CM", "email": "randy@email.com"},
+	{"username": "LimPeiXuan","password": "$argon2i$v=19$m=512,t=2,p=2$dIR+gSEKdzNyfc4O6ywpSw$oeuv/geCe3jGIQ7A7U/dsA","userType": "RM", "email": "peixuan@email.com"}
 	])
 
 // set up checklists table
